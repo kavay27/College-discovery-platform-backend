7526f3a5e9057a77ec2b9e67ccb37411d70230e8
kavay27 <kavaygupta27@gmail.com>
2026-05-26 21:03:44 +0530

    Expose root status endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -11,7 +11,9 @@ async function bootstrap() {
 
   app.use(helmet());
   app.enableCors();
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: '/', method: RequestMethod.GET }]
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
